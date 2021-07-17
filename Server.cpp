d2102c9d76a8a2cf3a873bd1358462eb7e9032d9
egums <dima110.ru@gmail.com>
2021-07-17 11:32:40 +0300

    add define, and describe sockaddr struct

--- Server.cpp
+++ Server.cpp
@@ -1,1 +1,19 @@
 #include "Server.hpp"
+
+int Server::start() {
+	return 0;
+}
+
+
+
+//TODO need adding port for that
+#define SOME_PORT 10000
+
+int Server::_socket_init() {
+	_m_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in sa_server {0};
+	sa_server.sin_addr.s_addr = INADDR_ANY;
+	sa_server.sin_family = AF_INET;
+	sa_server.sin_port = htons(SOME_PORT);
+}
--- Server.hpp
+++ Server.hpp
@@ -1,10 +1,38 @@
 #ifndef PROJECT_NAME_SERVER_HPP
 #define PROJECT_NAME_SERVER_HPP
 
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
 
 class Server {
+public:
+	int start();
 
+private:
+	int _socket_init();
+
+private:
+	int _m_socket;
+
+
+//	struct GradeTooLowException : public std::exception {
+//		GradeTooLowException() throw() {};
+//		virtual const char* what() const throw() {return "Grade is too low!";}
+//	};
 };
 
+
+struct Server_start_exception : public std::exception {
+	enum eWhat_messages {
+
+	};
+	std::string what_message;
+	Server_start_exception() throw() {};
+	virtual const char *what() const throw() { return "Grade is too high!";};
+};
 
 #endif //PROJECT_NAME_SERVER_HPP
